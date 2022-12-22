90bbe17e5d1524749eafc37412ed56ef92bffc16
vuggy17 <vuggy17@gmail.com>
2022-12-22 16:57:34 +0700

    remove old preload

--- src/renderer/ui/pages/preload/preload.tsx
+++ src/renderer/ui/pages/preload/preload.tsx
@@ -1,127 +1,176 @@
-// import { message as antMessage } from 'antd';
-// import axios from 'axios';
-// import { ReactNode, useEffect, useState } from 'react';
-// import { useRecoilState, useResetRecoilState } from 'recoil';
-// import { Message } from 'renderer/domain';
-// import {
-//   chatIdsState,
-//   useSetChatList,
-//   useUpdateChatItem,
-// } from 'renderer/hooks/use-chat';
-// import { chatMessagesState, useMessage } from 'renderer/hooks/use-chat-message';
-// import { currentUser } from 'renderer/hooks/use-user';
-// import { chatRepository } from 'renderer/repository/chat/chat.repository';
-// import SocketClient from 'renderer/services/socket';
-// import {
-//   EventListener,
-//   EventListenerWithAck,
-//   HasNewMessagePayload,
-//   PrivateMessageReceivedByPayload,
-//   ServerToClientEvent,
-// } from 'renderer/services/type';
-// import {
-//   addMessageToChat,
-//   getInitialChat,
-// } from 'renderer/usecase/conversation.usecase';
-// import {
-//   convertToPreview,
-//   sendMessageReceivedAck,
-// } from 'renderer/usecase/message.usecase';
+/* eslint-disable consistent-return */
+import { Spin, Typography } from 'antd';
+import { StringGradients } from 'antd/es/progress/progress';
+import { ReactNode, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { Message } from 'renderer/domain';
+import { userContacts } from 'renderer/hooks/contact-store';
+import { useChat, useChatMessage } from 'renderer/hooks/new-store';
 
-// export default function Preload({ children }: { children: ReactNode }) {
-//   const [appReady, setAppReady] = useState(true);
-//   const setList = useSetChatList();
-//   const { updateOrInsertMessage } = useMessage();
-//   const updateChatItem = useUpdateChatItem();
-//   const [user, setCurrentUser] = useRecoilState(currentUser);
+import { currentUser } from 'renderer/hooks/use-user';
+import { LocalDb } from 'renderer/services/localdb';
+import SocketClient from 'renderer/services/socket';
+import {
+  EventListener,
+  EventListenerWithAck,
+  HasNewMessagePayload,
+  PrivateMessageReceivedByPayload,
+  ServerToClientEvent,
+} from 'renderer/services/type';
+import {
+  addMessageToChat,
+  getInitialChat_v1,
+} from 'renderer/usecase/conversation.usecase';
+import {
+  convertToPreview,
+  sendMessageReceivedAck,
+} from 'renderer/usecase/message.usecase';
+import {
+  mapContactToChat,
+  syncChat,
+  syncContact,
+  syncMessage,
+} from 'renderer/utils/syncdata';
 
-//   const onHasNewMessage: EventListenerWithAck<HasNewMessagePayload> = (
-//     payload,
-//     ack
-//   ) => {
-//     const { chatId, message } = payload;
-//     console.log('[HAS_NEW_MESSAGE]: ', payload);
+// eslint-disable-next-line react/prop-types
+const Loader: React.FC<{ status: string }> = ({ status }) => {
+  return (
+    <div className="text-center w-full h-full flex items-center justify-center">
+      <div className="w-60 h-60 flex justify-center items-center rounded-lg bg-white shadow-sm">
+        <Spin tip={status} />
+      </div>
+    </div>
+  );
+};
 
-//     // DO NOT REMOVE SET TIMEOUT
-//     // settimeout to prevent recoil state update error
-//     setTimeout(() => {
-//       addMessageToChat(chatId, message as Message, {
-//         insertMessage: updateOrInsertMessage,
-//       });
+const LOADCOMPLETED = 'complete';
+export default function Preload({ children }: { children: ReactNode }) {
+  const [loadingStage, setLoadingStage] = useState<string>(
+    'Application initializing...'
+  );
+  const { insertMessage } = useChatMessage();
+  const { updateChat } = useChat();
+  const setUserContact = useSetRecoilState(userContacts);
+  const user = useRecoilValue(currentUser);
+  const navigate = useNavigate();
 
-//       updateChatItem({
-//         id: chatId,
-//         updates: {
-//           status: 'idle',
-//           lastMessage: convertToPreview(message),
-//           lastUpdate: message.createdAt,
-//         },
-//       });
-//     }, 100);
-//     // send ack signal
-//     if (user) {
-//       // if (typeof ack === 'function') ack({ chatId, message });
-//       sendMessageReceivedAck(message.sender.id, {
-//         chatId,
-//         receiverId: user.id,
-//         messageId: message.id,
-//       });
-//     }
-//   };
+  // init chat items
+  const { batchInitChats: setChatList } = useChat();
 
-//   const onMessageReceived: EventListener<PrivateMessageReceivedByPayload> = (
-//     payload: PrivateMessageReceivedByPayload
-//   ) => {
-//     const { chatId, messageId } = payload;
-//     updateOrInsertMessage(
-//       chatId,
-//       { id: messageId, status: 'received' },
-//       messageId
-//     );
-//   };
+  const onHasNewMessage: EventListenerWithAck<HasNewMessagePayload> = (
+    payload,
+    ack
+  ) => {
+    const { chatId, message } = payload;
+    console.log('[HAS_NEW_MESSAGE]: ', payload);
 
-//   const onMessageRead: EventListener<PrivateMessageReceivedByPayload> = (
-//     payload: PrivateMessageReceivedByPayload
-//   ) => {
-//     const { chatId, messageId } = payload;
-//     updateOrInsertMessage(chatId, { id: messageId, status: 'seen' }, messageId);
-//   };
+    // DO NOT REMOVE SET TIMEOUT
+    // settimeout to prevent recoil state update error
+    setTimeout(() => {
+      addMessageToChat(chatId, message as Message, {
+        insertMessage,
+      });
 
-//   useEffect(() => {
-//     SocketClient.addEventHandler(
-//       ServerToClientEvent.HAS_NEW_MESSAGE,
-//       onHasNewMessage
-//     );
-//     SocketClient.addEventHandler(
-//       ServerToClientEvent.MESSAGE_RECEIVED_BY,
-//       onMessageReceived
-//     );
-//     SocketClient.addEventHandler(
-//       ServerToClientEvent.SEEN_MESSAGE,
-//       onMessageRead
-//     );
-//     (async () => {
-//       // if (!user) {
-//       //   const res = await axios.post('/user/login', {
-//       //     username: 'Glennie_Swaniawski63',
-//       //     password: 'bSj2x325DhkjQqb',
-//       //   });
+      updateChat({
+        id: chatId,
+        updates: {
+          status: 'idle',
+          lastMessage: convertToPreview(message),
+          lastUpdate: message.createdAt,
+        },
+      });
+    }, 100);
+    // send ack signal
+    if (user) {
+      sendMessageReceivedAck(message.sender.id, {
+        chatId,
+        receiverId: user.id,
+        messageId: message.id,
+      });
+    }
+  };
 
-//       //   antMessage.success('Login success');
-//       //   setCurrentUser(res.data.data);
-//       // }
+  const onMessageReceived: EventListener<PrivateMessageReceivedByPayload> = (
+    payload: PrivateMessageReceivedByPayload
+  ) => {
+    const { chatId, messageId } = payload;
+    insertMessage(chatId, { id: messageId, status: 'received' }, messageId);
+  };
 
-//       const { data, extra } = await getInitialChat(chatRepository);
-//       setList({ data, extra });
+  const onMessageRead: EventListener<PrivateMessageReceivedByPayload> = (
+    payload: PrivateMessageReceivedByPayload
+  ) => {
+    const { chatId, messageId } = payload;
+    insertMessage(chatId, { id: messageId, status: 'seen' }, messageId);
+  };
 
-//       try {
-//         const res = await axios.get('/chat/total?page=1');
-//         console.log('res', res.data);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     })();
-//   }, []);
+  useEffect(() => {
+    // init local db
+    if (!user) return;
+    SocketClient.addEventHandler(
+      ServerToClientEvent.HAS_NEW_MESSAGE,
+      onHasNewMessage
+    );
+    SocketClient.addEventHandler(
+      ServerToClientEvent.MESSAGE_RECEIVED_BY,
+      onMessageReceived
+    );
+    SocketClient.addEventHandler(
+      ServerToClientEvent.SEEN_MESSAGE,
+      onMessageRead
+    );
+    (async () => {
+      setLoadingStage('Loading data...');
+      setUserContact(user.friends || []);
+      const response = await getInitialChat_v1();
+      setChatList(response.data, response.extra);
 
-//   return <>{appReady ? children : 'Loading'}</>;
-// }
+      requestIdleCallback(async () => {
+        if (user) {
+          setLoadingStage('Syncing data...');
+          const db = LocalDb.instance(user.userName);
+
+          if (user.friends && user.friends.length > 0) {
+            await db.open();
+            db.transaction(
+              'rw',
+              db.contacts,
+              db.chats,
+              db.privateChat,
+              db.messages,
+              async () => {
+                if (user.friends) syncContact(user.friends, db);
+                syncChat(response.data, db);
+                mapContactToChat(user, response.data, db);
+                response.data.forEach((chat) => syncMessage(chat.messages, db));
+                setLoadingStage(LOADCOMPLETED);
+              }
+            );
+            return 1;
+          }
+          return 1;
+        }
+        return 0;
+      });
+    })();
+
+    return () => {
+      console.log('users', user);
+      SocketClient.disconnect();
+      LocalDb.close();
+      navigate(0); // refresh recoil state
+    };
+  }, [user]);
+
+  return (
+    <div className="w-screen h-screen">
+      {loadingStage === LOADCOMPLETED ? (
+        children
+      ) : (
+        <Loader status={loadingStage} />
+      )}
+      ;
+    </div>
+  );
+}
