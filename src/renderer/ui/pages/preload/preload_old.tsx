18a2201d931bd19ff625fa83fc16b99f9f40c8b1
vuggy17 <vuggy17@gmail.com>
2023-01-07 19:53:37 +0700

    patch

--- src/renderer/ui/pages/preload/preload_old.tsx
+++ src/renderer/ui/pages/preload/preload_old.tsx
@@ -57,7 +57,7 @@ export default function Preload({
   const [loadingStage, setLoadingStage] = useState<string>(
     'Application initializing...'
   );
-  const { insertMessage } = useChatMessage();
+  const { insertMessage, appendMessage } = useChatMessage();
   const { updateChat, setChat } = useChat();
   const user = useRecoilValue(currentUser);
   const navigate = useNavigate();
@@ -86,9 +86,15 @@ export default function Preload({
         });
       }
 
-      addMessageToChat(chatId, message as Message, {
-        insertMessage,
-      });
+      if (!newChat) {
+        addMessageToChat(chatId, message as Message, {
+          insertMessage,
+        });
+      } else {
+        addMessageToChat(chatId, message as Message, {
+          insertMessage: appendMessage,
+        });
+      }
 
       updateChat({
         id: chatId,
@@ -144,10 +150,10 @@ export default function Preload({
       ServerToClientEvent.SEEN_MESSAGE,
       onMessageRead
     );
-    SocketClient.addEventHandler(
-      ServerToClientEvent.SEEN_MESSAGE,
-      onMessageRead
-    );
+    // SocketClient.addEventHandler(
+    //   ServerToClientEvent.SEEN_MESSAGE,
+    //   onMessageRead
+    // );
     SocketClient.addEventHandler(
       ServerToClientEvent.FRIEND_REQUEST_ACCEPTED,
       onFriendRequestAccepted
